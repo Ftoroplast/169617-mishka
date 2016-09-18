eacde4931ca317bd96ecc87addc0b63efe513d85
Keks <keks@htmlacademy.ru>
2016-09-18 06:04:33 +0300

    :kissing_cat: Добавил сборку precss в grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,16 @@
 "use strict";
 
 module.exports = function(grunt) {
-  grunt.loadNpmTasks("grunt-contrib-less");
   grunt.loadNpmTasks("grunt-browser-sync");
   grunt.loadNpmTasks("grunt-contrib-watch");
   grunt.loadNpmTasks("grunt-postcss");
 
   grunt.initConfig({
-    less: {
-      style: {
-        files: {
-          "css/style.css": "less/style.less"
-        }
-      }
-    },
-
     postcss: {
       style: {
         options: {
           processors: [
+            require("precss")(),
             require("autoprefixer")({browsers: [
               "last 1 version",
               "last 2 Chrome versions",
@@ -28,7 +20,8 @@ module.exports = function(grunt) {
             ]})
           ]
         },
-        src: "css/*.css"
+        src: "postcss/style.css",
+        dest: "css/style.css"
       }
     },
 
@@ -52,8 +45,8 @@ module.exports = function(grunt) {
 
     watch: {
       style: {
-        files: ["less/**/*.less"],
-        tasks: ["less", "postcss"],
+        files: ["postcss/**/*.css"],
+        tasks: ["postcss"],
         options: {
           spawn: false
         }
