b0869042b268607138a8c00ab917bb33bb8802e6
Ftoroplast <shpindler.ar@gmail.com>
2016-09-25 14:50:49 +1000

    Tablet->Desktop logo_page-header

--- js/script.js
+++ js/script.js
@@ -14,10 +14,15 @@
   document.body.removeNojsClass();
 
   var menu = document.querySelector(".menu");
+  var menuList = menu.querySelector(".menu__list");
   var menuBtn = menu.querySelector(".menu__btn");
   menuBtn.onclick = function () {
     menu.classList.toggle("menu_is-closed");
     menu.classList.toggle("menu_is-opened");
+    menuList.classList.toggle("menu__list_is-closed");
+    menuList.classList.toggle("menu__list_is-opened");
+    menuBtn.classList.toggle("menu__btn_is-closed");
+    menuBtn.classList.toggle("menu__btn_is-opened");
 
     return false;
   }
